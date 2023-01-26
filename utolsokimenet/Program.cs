2f17089d5d08734b0d9bfbe3fc214b23593b35f3
Marci02 <nagymarci.0218@gmail.com>
2023-01-26 13:21:11 +0100

    Add project files.

---
+++ utolsokimenet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace utolsokimenet
+{
+    class Program
+    {
+        static string SokszorVege(string szo, int szam)
+        {
+            foreach (var item in szo)
+            {
+                Console.Write(szo[szam]);
+            } 
+
+            
+            
+
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Adjon meg egy szót: ");
+            string szo = Console.ReadLine();
+
+            Console.Write("Adjon meg egy számot: ");
+            int szam = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(SokszorVege(szo, szam));
+
+            Console.ReadKey();
+        }
+    }
+}
