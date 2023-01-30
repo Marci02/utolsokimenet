01d0e47574b6a4e0511219a138a8c2bee317f073
Marci02 <nagymarci.0218@gmail.com>
2023-01-30 08:39:18 +0100

    megoldas

--- utolsokimenet/Program.cs
+++ utolsokimenet/Program.cs
@@ -8,27 +8,23 @@ namespace utolsokimenet
 {
     class Program
     {
-        static string SokszorVege(string szo, int szam)
-        {
-            foreach (var item in szo)
-            {
-                Console.Write(szo[szam]);
-            } 
-
-            
-            
-
-            
-        }
+        
         static void Main(string[] args)
         {
             Console.Write("Adjon meg egy szót: ");
             string szo = Console.ReadLine();
 
             Console.Write("Adjon meg egy számot: ");
-            int szam = Int32.Parse(Console.ReadLine());
+            int szam = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(SokszorVege(szo, szam));
+            for (int i = 0; i < szam+1; i++)
+            {
+                for (int j = szam; j > 0; j--)
+                {
+                    Console.Write(szo[szo.Length-j]);
+                }
+            }
+
 
             Console.ReadKey();
         }
